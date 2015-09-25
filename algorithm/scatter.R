d5f71d4156e44e9269884819cb5845faeb00380c
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-09-25 17:54:10 +0300

    Calculate distance matrix

--- algorithm/scatter.R
+++ algorithm/scatter.R
@@ -4,4 +4,10 @@
 
 scatter <- function(df) {
 
+    # Number of columns excluding label, which is the last
+    columns <- ncol(df) - 1
+
+    # Calculate distance matrix
+    distances <- as.matrix(dist(df[, 1:columns], method = "euclidean"))
+
 }
