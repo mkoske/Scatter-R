27cdd4a5f4225be98aeeb6dcef89171e9f3e78c9
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-11-09 14:00:19 +0200

    Minor formatting fixes and comments

--- algorithm/heom.R
+++ algorithm/heom.R
@@ -11,25 +11,22 @@ heom <- function(a, b, data, nominal = c()) {
     if(!is.vector(a) && !is.vector(b))
         stop("a and b, both, must be vectors")
 
-    # 1. either of values are missing, then distance is 1
-    # 2. overlap(xa, ya) if a is nominal or ordinal
-    # 3. rn_diff(xa, ya) otherwise
 
     if(length(a) != length(b))
         stop("Lengths of vectors a and b must be equal")
 
 
-    veclen = length(a)
-
+    len <- length(a)
     distances <- c() 
-    for(i in 1:veclen) {
+    for(i in 1:len) {
 
+        # Contains missing values
         if(is.na(a[i]) || is.na(b[i])) {
             distances <- c(distances, 1)
             next
         }
 
-        ## Nominal
+        ## Nominal (and ordinal) attributes
         if(any(i %in% nominal)) {
 
             if(a[i] == b[i])
@@ -40,7 +37,7 @@ heom <- function(a, b, data, nominal = c()) {
             next
         }
 
-        
+        # In all other cases, the distance is the ratio of difference and range 
         diff <- abs(a[i] - b[i])
         range <- max(data[, i]) - min(data[, i])
 
@@ -50,7 +47,6 @@ heom <- function(a, b, data, nominal = c()) {
 
     # sos = sum of squares
     sos <- sum(distances^2)
-
     return(sqrt(sos))
 }
 
