466c75b08dacf3185be6c757f04afc47e84bccc6
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-11-23 09:37:55 +0200

    Update test runner

--- test.R
+++ test.R
@@ -1,9 +1,5 @@
-require(clusterSim)
-source("./algorithm/scatter.R")
+source('./algorithm/scatter.R')
 
-raw <- read.csv('../testdata_3.csv')
-tdat <- data.Normalization(raw, type="n4")
-
-tdat$l <- c(1, 2, 2, 3)
-
+run(iris, distmethod = "heom")
+q()
 
