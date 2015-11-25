a5441515f8bd14081f351b865ac2381be5fe624f
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-11-25 14:46:52 +0200

    Update test runner

--- test.R
+++ test.R
@@ -6,7 +6,6 @@ raw <- read.csv('testdata_3.csv', sep = ',')
 df <- data.Normalization(raw[, 1:4], type="n4")
 df$class <- raw[, 5]
 
-dm <- distance(df, method = "heom")
+time <- system.time(dm <- distance(df, distmethod = "heom"))
 
-print(dm)
-
+print(time)
