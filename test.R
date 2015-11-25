efe8fc91ca6ccebc7570151cb6a5d063ca6c274e
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-11-25 13:44:54 +0200

    Update test runner

--- test.R
+++ test.R
@@ -1,5 +1,12 @@
+library(clusterSim)
 source('./algorithm/scatter.R')
 
-run(iris, distmethod = "heom")
-q()
+raw <- read.csv('testdata_3.csv', sep = ',')
+
+df <- data.Normalization(raw[, 1:4], type="n4")
+df$class <- raw[, 5]
+
+dm <- distance(df, method = "heom")
+
+print(dm)
 
