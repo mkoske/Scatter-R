3a758ff7f3cdfe0340673f942120b32b04ba07b6
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-12-14 09:55:01 +0200

    Fix run()-call

--- scatter_gui_handlers.R
+++ scatter_gui_handlers.R
@@ -3,27 +3,27 @@
 # GUI Handler: Show file dialog, read data from file, update main window based on data
 sgui$hand.fileOpen <- function(h, ...) {
 	sgui$sfile <- gfile(
-		type="open", 
+		type="open",
 		filter=list(
-			"CSV files"=list(patterns="*.csv"), 
+			"CSV files"=list(patterns="*.csv"),
 			"TXT files"=list(patterns="*.txt")
 	))
-	
+
 	sgui$sdata <- as.data.frame(read.csv(file=sgui$sfile))
 	sgui$sub.updateWithData()
 }
 
 sgui$hand.readDatafile <- function(h, ...) {
 	datafileWindow <- gwindow(title="Select datafile for read")
-	
+
 	cont.m <- ggroup(cont=datafileWindow, horizontal=FALSE, padding=20)
 	cont.a <- ggroup(cont=cont.m, horizontal=TRUE)
 	cont.b <- ggroup(cont=cont.m, horizontal=FALSE)
 	cont.c <- ggroup(cont=cont.m, horizontal=FALSE)
 	cont.d <- ggroup(cont=cont.m, horizontal=FALSE)
-	
+
 	sgui$sfile <- FALSE
-	
+
 	hand.selectfile <- function(h, ...) {
 		sgui$sfile <- gfile(
 			type="open",
@@ -34,14 +34,14 @@ sgui$hand.readDatafile <- function(h, ...) {
 		)
 		svalue(lbl_filename) <- sgui$sfile
 	}
-	
+
 	hand.readfile <- function(h, ...) {
 		if(sgui$sfile==FALSE) return()
 		sgui$sdata <- as.data.frame(read.csv(file=sgui$sfile, sep=func.separator_selToArg(svalue(rdo_separator))))
 		sgui$sub.updateWithData()
 		dispose(datafileWindow)
 	}
-	
+
 	func.separator_selToArg <- function(sel) {
 		if(sel==";") return (";")
 		if(sel==",") return (",")
@@ -49,8 +49,8 @@ sgui$hand.readDatafile <- function(h, ...) {
 		if(sel=="[tab]") return ("\t")
 		if(sel=="[space]") return ("")
 	}
-	
-	
+
+
 	btn_selectFile <- gbutton("Select file...", cont=cont.a, handler=hand.selectfile)
 	lbl_filename   <- glabel("No file selected", cont=cont.a)
 	lbl_separator  <- glabel("Select value separator",cont=cont.b)
@@ -68,7 +68,7 @@ sgui$sub.selectionVectorUpdate_class <- function() {
 	sgui$var.selected.classes  <- sgui$var.classes
 	sgui$var.attributes        <- sgui$var.allvariables[sgui$var.allvariables != sgui$var.classvar]
 	sgui$var.selected.attributes <- intersect(sgui$var.attributes, sgui$var.selected.attributes)
-	
+
 	# Attributes vector has changed, do update
 	sgui$sub.selectionVectorUpdate_attribute()
 }
@@ -89,28 +89,28 @@ sgui$sub.updateWithData <- function() {
 	colCount = ncol(sgui$sdata)
 	dataInfo = paste(sgui$sfile, " : ", rowCount, "rows with", colCount, "attributes")
 	svalue(sgui$lbl_datainfo) <- dataInfo
-	
+
 	# Initialize parameter selections
-	
+
 	## Invariants
 	sgui$var.allvariables     <- colnames(sgui$sdata)
 	sgui$var.allvariables_num <- sgui$var.allvariables[sapply(sgui$sdata, is.numeric)]
 	sgui$var.allvariables_int <- sgui$var.allvariables[sapply(sgui$sdata, is.integer)]
 	sgui$var.allvariables_fac <- sgui$var.allvariables[sapply(sgui$sdata, is.factor) ]
 	sgui$var.allvariables_flv <- union(sgui$var.allvariables_fac, sgui$var.allvariables_int)
-	
+
 	## Guess classvar and extract initial classes
 	sgui$var.classvar      <- colnames(sgui$sdata[sgui$guessClassvar(sgui$sdata)])
 	sgui$var.classes       <- levels(as.factor(sgui$sdata[[match(sgui$var.classvar, sgui$var.allvariables)]]))
 
 	## Do class selection update for selection vectors
 	sgui$sub.selectionVectorUpdate_class()
-	
+
 	## Initialize selection vectors for classes and attributes
 	sgui$var.selected.classes     <- sgui$var.classes
 	sgui$var.selected.attributes  <- sgui$var.attributes
-	
-	## Do attribute selection update for selection vectors 
+
+	## Do attribute selection update for selection vectors
 	sgui$sub.selectionVectorUpdate_attribute()
 
 	## Initialize selection vectors for binarized and scaled attributes
@@ -131,12 +131,12 @@ sgui$sub.updateSelectionView <- function() {
 	svalue(sgui$lbl_ppBinarized)  <- paste(sgui$var.selected.binarized,  collapse=", ")
 }
 
-# Guesses what might be the index of variable containing class information in dataframe 
+# Guesses what might be the index of variable containing class information in dataframe
 sgui$guessClassvar <- function(df) {
 	factors  = sapply(df, is.factor )
 	integers = sapply(df, is.integer)
 	numerics = sapply(df, is.numeric)
-	
+
 	for(i in length(factors ):1) if(factors[i])  return(i)
 	for(i in length(integers):1) if(integers[i]) return(i)
 	for(i in length(numerics):1) if(numerics[i]) return(i)
@@ -158,7 +158,7 @@ sgui$hand.select.classvar  <- function(h, ...) {
 			sgui$sub.updateSelectionView()
 			dispose(selWindow)
 		})
-		
+
 	radio <- gradio(sgui$var.allvariables, selected=match(sgui$var.classvar, sgui$var.allvariables), cont=cont.b)
 }
 
@@ -202,7 +202,7 @@ sgui$hand.select.binarized     <- function(h, ...) {
 	cont.a <- ggroup(cont=cont.m)
 	cont.b <- ggroup(cont=cont.m, use.scrollwindow=TRUE, horizontal=FALSE, expand=TRUE)
 	okButton <- gbutton("OK", cont=cont.a, hand=function(h,...) {
-		sgui$var.selected.binarized <- svalue(cbx) 
+		sgui$var.selected.binarized <- svalue(cbx)
 		sgui$var.selected.scaled    <- sgui$var.selected.scaled[! sgui$var.selected.scaled %in% intersect(sgui$var.selected.scaled, sgui$var.selected.binarized)]
 		sgui$sub.updateSelectionView()
 		dispose(selWindow)
@@ -256,27 +256,28 @@ sgui$hand.calculate <- function(h, ...) {
 		df                  = sgui$sdata,
 		classvar            = sgui$var.classvar,
 		included.attributes = sgui$var.selected.attributes,
-		binarized           = sgui$var.selected.binarized, 
+		binarized           = sgui$var.selected.binarized,
 		scaled              = sgui$var.selected.scaled,
 		na.action           = sgui$func.na.action_index_to_name(svalue(sgui$rdo_ppMissing, index=TRUE))
 	)
 
 	result <- run (
-		data       = sgui$ppdata, 
+		data       = sgui$ppdata,
 		classlabel = sgui$var.classvar,
-		distmethod = tolower(svalue(sgui$rdo_selectMethod)),
+		distanceMethod = tolower(svalue(sgui$rdo_selectMethod)),
 		iterations = svalue(sgui$spn_selectIterations),
+        usecase    = tolower(svalue(sgui$rdo_selectCalculation)),
+        baselineIterations = svalue(sgui$spn_selectBaselineIterations),
 		classes    = sgui$var.selected.classes,
 		columns    = sgui$var.selected.attributes
 	)
-	
+
 	# TODO do something with result
 	print(result)
-						 
+
 }
 
 # Print a vector with label/explanation in front of it
 sgui$func.printVector <- function(heading, vec) {
 	print(paste(heading, paste(vec, collapse=", ")))
-} 
-
+}
