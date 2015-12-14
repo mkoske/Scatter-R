cc3331776497309b1883a9bb99dd70c72567903c
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-12-14 15:01:41 +0200

    Update test runner

--- test.R
+++ test.R
@@ -1,9 +1,18 @@
 library(clusterSim)
 source('./algorithm/scatter.R')
 
-raw <- read.csv('testdata_3.csv', sep = ',')
+setosa <- sample(1:50, size = 4)
+versicolor <- sample(51:100, size = 4)
+virginica <- sample(101:150, size = 4)
 
-# Time with test data
-time <- system.time(dm <- distance(raw, distmethod = "heom", nominals = c(5, 7)))
+raw <- iris[c(setosa, versicolor, virginica), ]
 
-print(time)
+data <- data.Normalization(raw[,1:4], type="n4")
+data[, ncol(raw)] <- raw[, ncol(raw)]
+
+# distm.txt saved from this dm, like write.matrix(as.matrix(dm), "distm.txt")
+dm <- as.matrix(dist(data[, 1:4]))
+labels <- traverse(data, dm)
+
+write.matrix(dm, "distance-matrix.txt")
+print(labels)
