f82b8e7f4cd0936494ea9c812ffacd35dc3bcab3
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-12-17 14:03:31 +0200

    Add skeleton for HVDM implementation

---
+++ algorithm/hvdm.R
@@ -0,0 +1,11 @@
+hvdm <- function(a, b, ranges, nominal = c()) {
+
+    if(length(a) != length(b))
+        stop("Lengths of vectors a and b must be equal")
+
+    result <- NULL
+
+    # TODO: implement :)
+    
+    return(result)
+}
