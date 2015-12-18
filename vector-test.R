05f65d6bf8253cc8acad0c5b191b4aeb7499c599
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-12-18 16:09:15 +0200

    Update test file

--- vector-test.R
+++ vector-test.R
@@ -2,16 +2,18 @@
 # Trying to vectorize
 
 data <- read.csv("testdata_3.csv")
+ncols <- ncol(data) - 1
+data <- data[, 1:ncols] # Drop class labels
+fresult <- matrix(nrow = nrow(data), ncol = nrow(data))
+dlen <- nrow(data)
+start  <- proc.time()
+ranges <- vector(mode = "numeric", length = ncol(data))
+for(i in 1:dlen) {
+    if(i %% 10 == 0)
+        print("Next ten")
+    for(j in 1:dlen) {
+        a <- data[i, ]
+    }
+}
 
-a <- data[1, ]
-b <- data[2, ]
-
-print(a)
-print(b)
-
-which(a == b)       # Indices of those that are same
-as.numeric(a == b)  # Numeric vector, where 1 if theyr'e same, 0 if not
-
-as.numeric(is.na(a))# Like above, but 1, if a[i] is NA. This could be used to compare
-                    # items and finally to replace NA's with proper values (1)
-                    # like ... TODO: figure out an example
+print(proc.time() - start)
