007d068e20940be4eb5900bdf99b783bd8783d0f
Miika Koskela <miika.koskela@pp3.inet.fi>
2015-12-19 13:20:02 +0200

    Add TODO; remember to fix this

--- algorithm/heom.R
+++ algorithm/heom.R
@@ -1,41 +1,45 @@
 # ##
-# An implementation of HEOM metric. 
-# 
+# An implementation of HEOM metric.
+#
 # This creates distance matrix from given data.
-# 
+#
+# TODO: This breaks, if the data has only one column. Also, check if the sapply-
+# way to check numerics and factors is correct, since if there's only one column,
+# it returns logical vector for all cases, not for the column.
+#
 # For further reading, see [1].
-# 
+#
 # [1] Wilson, D. Randall, and Tony R. Martinez. 1997. “Improved Heterogeneous
 # Distance Functions.” Journal of Artificial Intelligence Research 6: 1–34.
 # doi:10.1613/jair.346.
-# ## 
+# ##
 heom <- function(data) {
-  
+
   ncols <- ncol(data) - 1
   data <- data[, 1:ncols]
-  
+
   factors <- sapply(data, is.factor)
   numerics <- sapply(data, is.numeric)
   ranges <- apply(data[, numerics], 2, max, na.rm = T) - apply(data[, numerics], 2, min, na.rm = T)
-  
+
   data <- sapply(data, as.numeric)
-  
+
   dlen <- nrow(data)
   result <- matrix(nrow = nrow(data), ncol = nrow(data))
   row <- vector(mode = "numeric", length = ncol(data))
-  
+
   for (i in 1:dlen) {
     for (j in 1:dlen) {
-      
+
       if(!is.na(result[i, j]))
         next
-      
+
       # For factor attributes, the distance is 1, if they're equal, 0 if not
       row[factors] <- !(data[i, factors] == data[j, factors])
-      
+
       # For numeric type, distance is xi - yi / range
       row[numerics] <- (data[i, numerics] - data[j, numerics]) / ranges
-      
+
       # For NA, distance is 1
       row[is.na(row)] <- 1
       ssum <- sum(row^2)
@@ -43,6 +47,6 @@ heom <- function(data) {
       result[j, i] <- ssum
     }
   }
-  
+
   return(result)
 }
