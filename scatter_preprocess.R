e271ad2e9829762a7b674673f14a990143550ed1
jmpaon <juha.panulaontto@gmail.com>
2016-01-07 19:27:37 +0200

    Added my_binarize: binarization function to replace DiscriMiner:binarize
    The DiscriMiner dependency can now be removed

--- scatter_preprocess.R
+++ scatter_preprocess.R
@@ -123,7 +123,44 @@ scatter.preprocess <- function(
 	
 	binarize_and_scale <- function(df, binarized=NULL, scaled=NULL) {
 		
-		require(DiscriMiner) # Binarization requires DiscriMiner package
+		my_binarize <- function(df) {
+			# Check that df is a dataframe with factors only, with no missing values
+			if(!is.data.frame(df)) stop("df must be a data frame")
+			if(!all(sapply(df, is.factor))) stop("df must be a data frame containning only factors")
+			if(!all(complete.cases(df))) stop("missing values are not allowed")
+
+			binarizedNames    <- function(df) {
+				return(unlist(Map(paste, colnames(df), 
+								lapply(df, levels), 
+								MoreArgs= list(sep=":")
+							),
+							use.names=FALSE))
+			}
+			
+			binNames <- binarizedNames(df)
+			nrows <- nrow(df)  #Number of rows in binarized dataframe
+			ncols <- ncol(df)  #Number of columns in binarized dataframe
+			categories <- sapply(df, nlevels)  #Number of levels for each column
+			categoriesCount <- sum(categories) #Number of levels for each column summed
+			
+			bdf <- matrix(0, nrows, categoriesCount)   #New matrix for results
+			i1 <- cumsum(categories) - categories + 1  #Insertion index
+			i2 <- cumsum(categories)                   #Insertion index
+			
+			for(i in 1:ncols)
+			{
+				lev <- levels(df[,i])
+				m   <- matrix(0, nrows, categories[i]) #Binarized matrix for single variable
+				for(ii in 1:categories[i]) {
+					temp <- df[,i] == lev[ii] #Rows of original df where value matches the value of new binarized column
+					m[temp,ii] <- 1 #Code all matches with 1, non-matches stay 0's
+				}
+				bdf[,i1[i]:i2[i]] <- m #Insert binarized single variable matrix to dataframe
+			}
+			
+			colnames(bdf) <- binNames #Add "binarized" names to binarized dataframe
+			return(bdf)
+		}
 		
 		# Merge two dataframes, column by column, *df2 coming after *df1
 		appendMerge <- function(df1, df2)
@@ -152,22 +189,9 @@ scatter.preprocess <- function(
 			if(ncol(df)==0) return(df)
 
 			fdf <- as.data.frame((sapply(df, as.factor))) # Mark all columns as factors
-			bdf <- binarize(fdf) # Call DiscriMiner::binarize to binarize all columns
-			colnames(bdf) <- binNames(fdf) # "Binarize" column names of binarized df
+			bdf <- my_binarize(fdf) # Binarize all columns of df
 			return(bdf)
 		}
-
-		# Return the names for binarized attributes
-		# In:  A dataframe with columns that will be binarized
-		# Out: A vector with column names for binarized variable columns
-		binNames    <- function(df) {
-			return(unlist(Map(paste, colnames(df), 
-							lapply(df, levels), 
-							MoreArgs= list(sep=":")
-						),
-						use.names=FALSE))
-		}
-
 
 		# Scale data frame to range 0..1
 		# In:  A dataframe with numeric attributes only
