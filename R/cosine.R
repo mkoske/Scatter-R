a119bade43385126d34111521c8b6956e4cd451d
Miika Koskela <miika.koskela@pp3.inet.fi>
2016-03-05 23:44:50 +0200

    Implement cosine measure

---
+++ R/cosine.R
@@ -0,0 +1,30 @@
+# ##
+#' An implementation of cosine measure.
+#'
+#' @param Data frame
+#' @return Distance matrix
+#' @export
+# ##
+cosine <- function(data) {
+
+    n <- nrow(data)
+    p <- ncol(data)
+    result <- matrix(nrow = n, ncol = n)
+
+    for (i in 1:n) {
+        for (j in 1:n) {
+
+            if(!is.na(result[i, j]))
+                next
+
+            a <- as.numeric(data[i,])
+            b <- as.numeric(data[j,])
+
+            sim <- 1 - ((a %*% b) / (sqrt(a %*% a) * sqrt(b %*% b)))
+            result[i, j] <- sim
+            result[j, i] <- sim
+        }
+
+    }
+    result
+}
